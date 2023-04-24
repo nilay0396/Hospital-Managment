c7342d21e2e50b2f4caa04c6e5c8eb60fc9031a1
nilay <nilay0396@gmail.com>
2023-04-24 10:09:17 +0530

    2nd commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+$(document).ready(function() {
+    // Toggle nav menu on click
+    $('#navToggle').on('click', function() {
+      $('#navMenu').toggleClass('show');
+    });
+  });
+  
